fb1727044def0feaa13fe6fbdc51ae477cce2ae4
Anjali <anjaliramesh14012005@gmail.com>
2026-02-07 22:43:33 +0530

    oops

---
+++ Oops/Student.java
@@ -0,0 +1,11 @@
+package Oops;
+
+public class Student {
+    int yob ;
+    String name;
+
+    public int check(){
+        System.out.println("executed");
+        return yob;
+    }
+}
---
+++ Oops/day.java
@@ -0,0 +1,13 @@
+package Oops;
+
+public class day {
+    public static void main(String[] args) {
+        
+        Student s1 = new Student();
+
+        // s1.yob = 21;
+        // s1.name = "anjali";
+        System.out.println(s1.yob+" "+s1.name);
+
+    }
+}
